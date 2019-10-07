4befb57805ca41eeee29c90cfa601158f4bb76b7
Evan Peter Oijordsbakken <epo40@cs18266kq.canterbury.ac.nz>
2019-10-08 12:24:31 +1300

    startup screen

--- game.c
+++ game.c
@@ -1,14 +1,37 @@
 #include "system.h"
+#include "pacer.h"
+#include "navswitch.h"
+#include "ir_uart.h"
+#include "tinygl.h"
+#include "../fonts/font5x7_1.h"
+
+
+#define PACER_RATE 500
+#define MESSAGE_RATE 20
+
+void startup(void)
+{
+    tinygl_init (PACER_RATE);
+    tinygl_font_set (&font5x7_1);
+    tinygl_text_speed_set (MESSAGE_RATE);
+    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
+
+    tinygl_text("P,S,R. press nav to begin ");
+
+    pacer_init (PACER_RATE);
+
+    while (1) {
+
+        pacer_wait();
+
+        tinygl_update();
+    }
+}
 
 int main (void)
 {
-    system_init ();
+    system_init();
+    startup();
 
-
-    while (1)
-    {
-
-
-
-    }
+    return 0;
 }
