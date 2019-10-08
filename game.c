fd5b7e98caf649310e0169931f1c00a82a9931ea
Evan Peter Oijordsbakken <epo40@cs18265kq.canterbury.ac.nz>
2019-10-08 16:11:00 +1300

    IR checks both players readyness before continuing

--- game.c
+++ game.c
@@ -9,7 +9,9 @@
 #define PACER_RATE 500
 #define MESSAGE_RATE 20
 
-void display_character (char character)
+/* Commonly used helper functions */
+
+void display_character(char character)
 {
     char buffer[2];
     buffer[0] = character;
@@ -17,38 +19,43 @@ void display_character (char character)
     tinygl_text (buffer);
 }
 
-/* Displays the welcome message for the game, including instructions
- * Then waits for a connection to a second player.
-*/
-void startup_task(void)
+/* Helper functions for startup_task() */
+
+void startup_screen(void) //Displays game title and waits for player interaction
 {
-    tinygl_init (PACER_RATE);
-    tinygl_font_set (&font5x7_1);
-    tinygl_text_speed_set (MESSAGE_RATE);
-    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
-    pacer_init(PACER_RATE);
-    
+    int cont = 0;
 
     tinygl_text("Paper Scissors Rock, best of 3. Push navswitch to continue.");
-    
 
-    while (1) {
-        pacer_wait();
-        tinygl_update();
-        if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
-            break;
-        }
-    }
-    
-    char character = 'Y';
-    display_character(character);
-    pacer_init (PACER_RATE);
-    
-    while (1) {
+
+    while (!cont) {
+
         pacer_wait();
         tinygl_update();
         navswitch_update();
-        
+
+        if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
+            cont = 1;
+        }
+    }
+}
+
+void ready_screen(void) //Displays player ready screen and waits for player2
+{
+    int cont = 0;
+    char character = 'Y';
+    char player2_char = 'N';
+    int rdy1 = 0;
+    int rdy2 = 0;
+
+    display_character(character);
+
+    while (!cont) {
+
+        pacer_wait ();
+        tinygl_update ();
+        navswitch_update ();
+
         if (navswitch_push_event_p (NAVSWITCH_EAST) || navswitch_push_event_p (NAVSWITCH_WEST)) {
             if (character == 'Y') {
                 character = 'N';
@@ -56,14 +63,56 @@ void startup_task(void)
                 character = 'Y';
             }
         }
+
+        if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
+            ir_uart_putc(character);
+            if (character == 'Y') {
+                rdy1 = 1;
+            } else {
+                rdy1 = 0;
+            }
+        }
+
+        if (ir_uart_read_ready_p ()) {
+            player2_char = ir_uart_getc ();
+            if (player2_char == 'Y') {
+                rdy2 = 1;
+            } else {
+                rdy2 = 0;
+            }
+        }
+
+        if (rdy1 && rdy2) {
+            cont = 1;
+        }
+
         display_character(character);
     }
+}
+
+
+/* Displays the welcome message for the game, including instructions
+ * Then waits for a connection to a second player.
+ */
+void startup_task(void)
+{
+    tinygl_init (PACER_RATE);
+    tinygl_font_set (&font5x7_1);
+    tinygl_text_speed_set (MESSAGE_RATE);
+    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
+    pacer_init(PACER_RATE);
+    ir_uart_init ();
+
+    startup_screen();
+    ready_screen();
 }
 
 int main (void)
 {
     system_init();
     startup_task();
+    tinygl_clear();
+    tinygl_update ();
 
     return 0;
 }
