34b2de071d6d032c2aa91264d1e40463a45c6473
Ryan Robert Miller <rrm36@cs18211kq.canterbury.ac.nz>
2019-10-09 14:45:06 +1300

    Added score module, not complete, may not be worth tbh. Changed ready screen to return a success value and updated some comments

---
+++ score.c
@@ -0,0 +1,8 @@
+/* @file score.c
+    @authors R. Miller, E. Oijordsbakken
+    @date 9 October 2019
+    @brief Score Keeping
+*/
+
+#include "button.h"
+#include "led.h"
---
+++ score.h
@@ -0,0 +1,12 @@
+/* @file score.c
+    @authors R. Miller, E. Oijordsbakken
+    @date 9 October 2019
+    @brief Score Keeping
+*/
+
+#ifndef SCORE_H
+#define SCORE_H
+
+
+#include "button.h"
+#include "led.h"
