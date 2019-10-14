5c4094f1800470bfe5cb7572fa8607bbfc550351
Ryan Robert Miller <rrm36@cs18271kq.canterbury.ac.nz>
2019-10-14 16:07:09 +1300

    Removed functions that were bundled into modules.

--- game.c
+++ game.c
@@ -5,7 +5,7 @@
     This file runs the main program of the PSR game
     for the UCFK for Assignment
 */
-
+#include <string.h>
 #include "system.h"
 #include "pacer.h"
 #include "ir_uart.h"
@@ -14,25 +14,12 @@
 #include "../fonts/font5x7_1.h"
 #include "round.h"
 #include "ready.h"
+#include "messages.h"
 
-
-#define PACER_RATE 800
+#define PACER_RATE 1000
 #define MESSAGE_RATE 25
 #define MAX_SCORE 3
-void start_screen(void)
-{
-    tinygl_text("Paper Scissors Rock: First to 3");
 
-    while (1) {
-        pacer_wait();
-        tinygl_update();
-        navswitch_update();
-
-        if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
-            break;
-        }
-    }
-}
 
 void startup_init(void)
 {
@@ -45,48 +32,10 @@ void startup_init(void)
     tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
 }
 
-void ready_task(char* stage)
-{
-    static char p1_ready = 'Y';
-    static char p2_ready = 'N';
-    static uint8_t sent_Y = 0;
-
-    display_character(p1_ready);
-    change_selection(&p1_ready);
-
-    if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
-        ir_uart_putc(p1_ready);
-        if (p1_ready == 'Y') {
-            sent_Y = 1;
-        } else {
-            sent_Y = 0;
-        }
-    }
-    if (ir_uart_read_ready_p()) {
-        p2_ready = ir_uart_getc();
-    }
-    if (sent_Y && p2_ready == 'Y') {
-        p2_ready = 'N';
-        sent_Y = 0;
-        *stage = 'P';
-    }
-}
-
-void change_selection(char* p1_ready)
-{
-    if (navswitch_push_event_p(NAVSWITCH_EAST) || navswitch_push_event_p(NAVSWITCH_WEST)) {
-        if (*p1_ready == 'Y') {
-            *p1_ready = 'N';
-        } else {
-            *p1_ready = 'Y';
-        }
-    }
-}
-
 int main(void)
 {
     startup_init();
-    start_screen();
+    display_message("Paper Scissors Rock: First to 3");
 
     char stage = 'R';
     uint8_t p1_score = 0;
@@ -107,6 +56,7 @@ int main(void)
                 break;
 
             case 'W' : // Round was won
+                display_message("Won round!");
                 p1_score++;
                 if (p1_score == MAX_SCORE) {
                     stage = 'Q';
@@ -116,6 +66,7 @@ int main(void)
                 break;
 
             case 'L' : // Round was lost
+                display_message("Lost round!");
                 p2_score++;
                 if (p2_score == MAX_SCORE) {
                     stage = 'Q';
@@ -125,6 +76,7 @@ int main(void)
                 break;
 
             case 'D' : // Round was a draw
+                display_message("Draw!");
                 stage = 'R';
                 break;
         }
