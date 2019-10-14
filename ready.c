a52b8b5f2434e04234cdd6fea5736ad3a97d0283
Ryan Robert Miller <rrm36@cs18271kq.canterbury.ac.nz>
2019-10-14 16:03:45 +1300

    Added Ready module.

---
+++ ready.c
@@ -0,0 +1,52 @@
+/** @file round.c
+    @authors Evan Oijordsbakken, Ryan Miller
+    @date   11 October 2019
+    @brief Ready check
+
+*/
+
+#include "ready.h"
+#include "system.h"
+#include "ir_uart.h"
+#include "tinygl.h"
+#include "navswitch.h"
+#include "messages.h"
+
+
+void ready_task(char* stage)
+{
+    static char p1_ready = 'Y';
+    static char p2_ready = 'N';
+    static uint8_t sent_Y = 0;
+
+    display_character(p1_ready);
+    change_selection(&p1_ready);
+
+    if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+        ir_uart_putc(p1_ready);
+        if (p1_ready == 'Y') {
+            sent_Y = 1;
+        } else {
+            sent_Y = 0;
+        }
+    }
+    if (ir_uart_read_ready_p()) {
+        p2_ready = ir_uart_getc();
+    }
+    if (sent_Y && p2_ready == 'Y') {
+        p2_ready = 'N';
+        sent_Y = 0;
+        *stage = 'P';
+    }
+}
+
+void change_selection(char* p1_ready)
+{
+    if (navswitch_push_event_p(NAVSWITCH_EAST) || navswitch_push_event_p(NAVSWITCH_WEST)) {
+        if (*p1_ready == 'Y') {
+            *p1_ready = 'N';
+        } else {
+            *p1_ready = 'Y';
+        }
+    }
+}
---
+++ ready.h
@@ -0,0 +1,22 @@
+/** @file round.h
+    @authors Evan Oijordsbakken, Ryan Miller
+    @date   11 October 2019
+    @brief  Ready check
+
+    This module allows the functionality for a player selecting whether or not
+    they're ready to play a round.
+    Module implements the choice selection, and the recieving of the other players
+    choice.
+
+*/
+#ifndef READY_H
+#define READY_H
+
+
+
+void ready_task(char* stage);
+
+void change_selection(char* p1_ready);
+
+
+#endif
